bbdaff2c2d9f2f30e134ef0dcab5a1e06b745e82
bgu303 <bgu303@myy.haaga-helia.fi>
2023-01-15 15:02:58 +0200

    hmm

--- Calculator.js
+++ Calculator.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TESTIÄ</Text>
+      <Text>TESTIÄASDLÖASDASDADSDASD</Text>
       <Text>AAAA</Text>
     </View>
   );
