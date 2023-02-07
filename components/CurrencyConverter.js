d3e811df9a2c877568a645653166540e28b2b5fc
bgu303 <bgu303@myy.haaga.helia.fi>
2023-02-07 13:39:24 +0200

    small fixes

--- components/CurrencyConverter.js
+++ components/CurrencyConverter.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, TextInput, FlatList, Image } from 'react-native';
+import { View, Text, Button, TextInput, ActivityIndicator } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { API_TOKEN } from '@env'
 
@@ -9,8 +9,10 @@ export default function CurrencyExchange() {
     const [symbols, setSymbols] = useState([])
     const [selectedSymbol, setSelectedSymbol] = useState();
     const [result, setResult] = useState("")
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         fetch("https://api.apilayer.com/exchangerates_data/symbols", {
             headers: {
                 "apikey": `${API_TOKEN}`
@@ -19,11 +21,13 @@ export default function CurrencyExchange() {
             .then(response => response.json())
             .then(result => {
                 setSymbols(Object.keys(result.symbols))
+                setLoading(false)
             })
             .catch(error => console.log('error', error));
     }, [])
 
     const fetchData = () => {
+        setLoading(true)
         fetch(`https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=${selectedSymbol}&amount=${amount}`, {
             headers: {
                 "apikey": `${API_TOKEN}`
@@ -32,12 +36,14 @@ export default function CurrencyExchange() {
             .then(response => response.json())
             .then(result => {
                 setResult(`Converted to Euros: ${result.result.toFixed(2)}`)
+                setLoading(false)
             })
             .catch(error => console.log('error', error));
     }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 80 }}>
+            <ActivityIndicator size="small" animating={loading}/>
             <Text style={{fontSize: 20}}>{result}</Text>
             <TextInput
                 style={{ width: 150, borderColor: 'gray', borderWidth: 1, marginBottom: 15 }}
